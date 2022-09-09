f0e32b19e2a798cb9780ee08ae38062698ec38f0
Tomislav Preksavec <tomislav.preksavec@gmail.com>
2022-09-09 14:20:58 +0200

    Add credentials template file

---
+++ infotron/include/CredentialsTemplate.h
@@ -0,0 +1,39 @@
+/*******************************************************************
+ * IMPORTANT! Please follow the instructions in this file before
+ * you try to compile the project for the first time. This file
+ * is just a template, don't change the contents of this file.
+ * 
+ * You need to do the following:
+ * 1. Save this file as Credentials.h in include/ directory and
+ *    than close this file
+ * 2. Open Credentials.h file and enter new SSID and PASS for 
+ *    Infotron device - you'll use this to connect directly to 
+ *    Infotron, even when there is no WiFi network you can use
+ * 3. Open Credentials.h file and enter your WiFi credentials under
+ *    WIFI_SSID and WIFI_PASS
+ *    (you can add alternate credentials or leave them commented out)
+ * 
+ * Credentials.h is ignored by Git (check the contents of .gitignore
+ * file to confirm), so this way your credentials will be kept in
+ * your local version of the project. This way you can change the 
+ * project and publish your changes on Github, Gitlab, Bitbucket or
+ * your own Git server without leaking sensitive information.
+ *******************************************************************/
+
+#ifndef CREDENTIALS_H
+    #define CREDENTIALS_H
+    // Replace with actual SSID and password for Infotron device 
+    // (choose anything you want):
+    #define INFOTRON_SSID "Infotron SSID here"
+    #define INFOTRON_PASS "Infotron AP password here"
+
+    // Replace with actual SSID and password for you WLAN:
+    #define WIFI_SSID "WiFi SSID here"
+    #define WIFI_PASS "WiFi password here"
+
+    // If you can use alternate network in case the first one is
+    // unreachable, uncomment lines bellow and enter credentials
+    //#define ALTERNATE_WIFI_SSID "Alternate WiFi SSID here"
+    //#define ALTERNATE_WIFI_PASS "Alternate WiFi password here"
+
+#endif
