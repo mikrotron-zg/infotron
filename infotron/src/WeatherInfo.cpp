2c6b5f25d35d5c3ef617365855109318625aecc6
Tomislav Preksavec <tomislav.preksavec@gmail.com>
2023-05-11 13:36:49 +0200

    Update license and comments for WeatherInfo component

--- infotron/src/WeatherInfo.cpp
+++ infotron/src/WeatherInfo.cpp
@@ -1,3 +1,24 @@
+/*
+ * This file is part of Infotron project (https://github.com/mikrotron-zg/infotron)
+ * developed by Mikrotron d.o.o. (http://mikrotron.hr).
+ * It contains basic weather info handler for OpenWeather 
+ * (https://openweathermap.org/) API.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. See the LICENSE file at the 
+ * top-level directory of this distribution for details 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <HTTPClient.h>
 #include <Arduino_JSON.h>
 #include "Configuration.h"
@@ -6,6 +27,7 @@
 
 
 void parseWeatherInfo(String input) {
+    // Parse received JSON file
     JSONVar root = JSON.parse(input);
     if (JSON.typeof(root) == "undefined") {
         DEBUGLN("Parsing input failed!");
--- infotron/src/WeatherInfo.h
+++ infotron/src/WeatherInfo.h
@@ -1,6 +1,23 @@
-/*******************************************************************
- * Weather info handler, 
- *******************************************************************/
+/*
+ * This file is part of Infotron project (https://github.com/mikrotron-zg/infotron)
+ * developed by Mikrotron d.o.o. (http://mikrotron.hr).
+ * It contains basic weather info handler for OpenWeather 
+ * (https://openweathermap.org/) API.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. See the LICENSE file at the 
+ * top-level directory of this distribution for details 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #ifndef WEATHER_INFO_H
     #define WEATHER_INFO_H
@@ -10,6 +27,10 @@
     #define LONGITUDE 15.98
 
     #include "Credentials.h" // please read the instructions in include/CredentialsTemplate.h file
-    // Public functions
+    /**
+     * @brief Gets weather info from OpenWeather API
+     * 
+     * Connects to OpenWeather server, downloads and parses received JSON file.
+     */
     void getWeatherInfo();
 #endif
